0e5ebdf77e51523ee06d9d2692803c17a445ed7d
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-24 10:45:56 +0800

    Fix: Build error in colors, extend colors tailwind

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -3,28 +3,28 @@ import React from "react";
 const schedules = [
   {
     day: 'Tuesday',
-    color: '#f2f5f2',
+    color: 'color1',
     textColor: 'text-red-500',
     subs : ['Network Technology and Architecture', 'Computing Theory'],
     time: ['9:30am - 12:00pm','12:00pm - 2:00']
   },
   {
     day: 'Wednesday',
-    color: '#fee9e9',
+    color: 'color2',
     textColor: 'text-green-800',
     subs : ['Calculus', 'Network Technology and Architecture','Parallel & Visual Computing','Network Principles'],
     time: ['8:00am - 9:30am','9:30am - 12:00pm','12:30pm - 3:00pm','6:00pm - 8:00pm']
   },
   {
     day: 'Thursday',
-    color: '#e6f3f3',
+    color: 'color3',
     textColor: 'text-green-800',
     subs : ['Network Principles and Programming','Web Programming','Calculus','Computing Theory'],
     time: ['9:30am -12:00pm','12:30pm - 3:00pm','6:00pm - 7:30pm','']
   },
   {
     day: 'Friday',
-    color:'#f2f5f2',
+    color:'color1',
     textColor: 'text-blue-900',
     subs : ['Parallel & Visual Computing', 'Web Programing','Software Engineering'],
     time: ['9:30am -12:00pm', '12:30am - 3:00pm','6:00pm - 8:30pm']
@@ -35,7 +35,7 @@ export default function daily() {
   return (
     <div>
       {schedules.map(({day,subs,time,color,textColor}) => (
-        <div className = {`grid md:grid-cols-8 font-secondary font-bold mx-1 md:mx-20 h-32 rounded-lg mt-1 bg-[${color}]`}>
+        <div className = {`grid md:grid-cols-8 font-secondary font-bold mx-1 md:mx-20 h-32 rounded-lg mt-1 bg-${color}`}>
             <h2 className = {`md:pt-2 md:pl-4 text-sm md:text-lg ${textColor} justify-self-start`}>{day}</h2>
             <div className = "text-xs md:text-base font-normal col-start-3 col-span-2 self-center">
               {subs.map((sub) => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   purge: ['./index.html', './src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      color1: '#f2f5f2',
+      color2: '#fee9e9',
+      color3: '#e6f3f3',
+    }),
     extend: {
       fontFamily: {
         primary: "'Bebas Neue', cursive",
