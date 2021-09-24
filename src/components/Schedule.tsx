8ba9b60069414eb99668bc669cf613ccaddfe5fb
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-24 10:32:06 +0800

    Edit mispelled text

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -12,21 +12,21 @@ const schedules = [
     day: 'Wednesday',
     color: '#fee9e9',
     textColor: 'text-green-800',
-    subs : ['Calculus', 'Network Technology and Architecture','CS Elective 2','Network Principles'],
+    subs : ['Calculus', 'Network Technology and Architecture','Parallel & Visual Computing','Network Principles'],
     time: ['8:00am - 9:30am','9:30am - 12:00pm','12:30pm - 3:00pm','6:00pm - 8:00pm']
   },
   {
     day: 'Thursday',
     color: '#e6f3f3',
     textColor: 'text-green-800',
-    subs : ['Network Principles','Web Programming','Calculus','Computing Theory'],
+    subs : ['Network Principles and Programming','Web Programming','Calculus','Computing Theory'],
     time: ['9:30am -12:00pm','12:30pm - 3:00pm','6:00pm - 7:30pm','']
   },
   {
     day: 'Friday',
     color:'#f2f5f2',
     textColor: 'text-blue-900',
-    subs : ['CS Elective 2', 'Web Programing','Software Engineering'],
+    subs : ['Parallel & Visual Computing', 'Web Programing','Software Engineering'],
     time: ['9:30am -12:00pm', '12:30am - 3:00pm','6:00pm - 8:30pm']
   },
 ];
@@ -35,16 +35,16 @@ export default function daily() {
   return (
     <div>
       {schedules.map(({day,subs,time,color,textColor}) => (
-        <div className = {`grid grid-cols-8 font-secondary font-bold mx-20 h-32 rounded-lg mt-1 bg-[${color}]`}>
-            <h2 className = {`pt-2 pl-4 text-lg ${textColor}`}>{day}</h2>
-            <div className = "mt-3 text-normal font-normal col-start-3 col-span-2">
+        <div className = {`grid md:grid-cols-8 font-secondary font-bold mx-1 md:mx-20 h-32 rounded-lg mt-1 bg-[${color}]`}>
+            <h2 className = {`md:pt-2 md:pl-4 text-sm md:text-lg ${textColor} justify-self-start`}>{day}</h2>
+            <div className = "text-xs md:text-base font-normal col-start-3 col-span-2 self-center">
               {subs.map((sub) => {
-                return <p>{sub}</p>
+                return <p className = 'w-full'>{sub}</p>
               })}
             </div>
-            <div className = "mt-3 col-start-6 text-normal font-normal">
+            <div className = "col-start-6 text-xs md:text-base font-normal self-center">
               {time.map((time) => {
-                return <p>{time}</p>
+                return <p className = 'w-full'>{time}</p>
               })}
             </div>
         </div>
