cd62f772b8575dff35919a785a6b71c85eacff8b
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-24 10:31:35 +0800

    Add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Schedules from './components/Schedule'
+import Footer from './components/Footer'
 
 
 const App:React.FC = () => {
@@ -8,6 +9,7 @@ const App:React.FC = () => {
     <div className="mx-auto w-full">
       <Header/>
       <Schedules/>
+      <Footer/>
     </div>
   )
 }
