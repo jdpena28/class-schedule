1bb99ad1d42de72d8ef30d3a6a277fc4f03e3fc6
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-24 13:32:12 +0800

    Feature: add responsive queries for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const  Header:React.FC = () => {
   return (
-    <header className = "bg-red-500 p-5 mt-5 md:mx-20 mx-1">
+    <header className = "bg-red-500 p-5 mt-1 md:mt-5 md:mx-20 mx-1">
       <h1 className = "font-secondary text-4xl font-bold text-center text-[#f2f5f2]"><b className="text-black">ND3A</b> CLASS SCHEDULE</h1>
     </header>
   )
--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -5,38 +5,44 @@ const schedules = [
     day: 'Tuesday',
     color: 'color1',
     textColor: 'text-red-500',
-    subs : ['Network Technology and Architecture', 'Computing Theory'],
-    time: ['9:30am - 12:00pm','12:00pm - 2:00']
+    subs : ['Network Technology and Architecture', 'Computing Theory','Software Engineering'],
+    time: ['9:30am - 12:00pm','12:00pm - 2:00','6:00pm - 8:30pm'],
+    link: ['','https://meet.google.com/grn-mwie-qsi','meet.google.com/tbr-tpye-iyx']
   },
   {
     day: 'Wednesday',
     color: 'color2',
     textColor: 'text-green-800',
     subs : ['Calculus', 'Network Technology and Architecture','Parallel & Visual Computing','Network Principles'],
-    time: ['8:00am - 9:30am','9:30am - 12:00pm','12:30pm - 3:00pm','6:00pm - 8:00pm']
+    time: ['8:00am - 9:30am','9:30am - 12:00pm','12:30pm - 3:00pm','6:00pm - 8:00pm'],
+    link: ['','https://meet.google.com/wdq-bvne-zbd','','']
   },
   {
     day: 'Thursday',
     color: 'color3',
     textColor: 'text-green-800',
     subs : ['Network Principles and Programming','Web Programming','Calculus','Computing Theory'],
-    time: ['9:30am -12:00pm','12:30pm - 3:00pm','6:00pm - 7:30pm','']
+    time: ['9:30am -12:00pm','12:30pm - 3:00pm','3:30pm - 5:00pm','6:00pm - 7:30pm'],
+    link: ['','','','']
   },
   {
     day: 'Friday',
     color:'color1',
     textColor: 'text-blue-900',
     subs : ['Parallel & Visual Computing', 'Web Programing','Software Engineering'],
-    time: ['9:30am -12:00pm', '12:30am - 3:00pm','6:00pm - 8:30pm']
+    time: ['9:30am -12:00pm', '12:30am - 3:00pm','6:00pm - 8:30pm'],
+    link: ['','https://meet.google.com/tuu-dhjc-dgf','']
   },
 ];
 
 export default function daily() {
   return (
     <div>
-      {schedules.map(({day,subs,time,color,textColor}) => (
-        <div className = {`grid md:grid-cols-8 font-secondary font-bold mx-1 md:mx-20 h-32 rounded-lg mt-1 bg-${color}`}>
-            <h2 className = {`md:pt-2 md:pl-4 text-sm md:text-lg ${textColor} justify-self-start`}>{day}</h2>
+      {schedules.map(({day,subs,time,link,color,textColor}) => (
+        <>
+        <h2 className = {`md:hidden font-secondary font-bold mx-1 text-sm md:text-lg ${textColor} justify-self-start`}>{day}</h2>
+        <div className = {`grid md:grid-cols-8 font-secondary font-bold mx-1 md:mx-20 h-32 rounded-lg mt-1 ${color}`}>
+            <h2 className = {`h2tag md:pt-2 md:pl-4 text-sm md:text-lg ${textColor} justify-self-start`}>{day}</h2>
             <div className = "text-xs md:text-base font-normal col-start-3 col-span-2 self-center">
               {subs.map((sub) => {
                 return <p className = 'w-full'>{sub}</p>
@@ -47,7 +53,23 @@ export default function daily() {
                 return <p className = 'w-full'>{time}</p>
               })}
             </div>
+            <div className = 'col-start-8 text-xs md:text-base font-normal self-center'>
+              {
+                link.map((link) => {
+                  if (link !== '') {
+                    return <a href={`${link}`}><p className ='underline text-blue-800'>G-Meet Link</p></a>
+                  }
+                  else {
+                    return <p></p>
+                  }
+                })
+
+              /* {link.map((link) => {
+                return <a href={`${link}`}><p className = 'underline text-blue-800'>G-Meet Link</p></a>
+              })} */}
+            </div>
         </div>
+      </>
       ))}
     </div>
   );
