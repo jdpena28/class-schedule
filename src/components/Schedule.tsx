617515d20576882b4afc2d1f75a4804a585ca4cc
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-04 21:08:13 +0800

    Fix class schedule

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -16,7 +16,7 @@ const schedules = [
     textColor: 'text-green-800',
     subs : ['Calculus', 'Network Technology and Architecture','Parallel & Visual Computing','Network Principles'],
     time: ['8:00am - 9:30am','9:30am - 12:00pm','12:30pm - 3:00pm','6:00pm - 8:00pm'],
-    link: ['https://meet.google.com/bos-evnc-vvd','https://meet.google.com/qpg-zfhr-znw','https://meet.google.com/zxn-cxxw-cxu']
+    link: ['https://meet.google.com/bos-evnc-vvd','https://meet.google.com/qpg-zfhr-znw','https://meet.google.com/zxn-cxxw-cxu','']
   },
   {
     day: 'Thursday',
